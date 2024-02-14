2aed785eadbb92717fa38e941221b97b1d4613a7
illsk1lls <gene.rovinsky@gmail.com>
2024-02-14 00:10:36 -0500

    Update PowerPlayer.ps1

--- PowerPlayer.ps1
+++ PowerPlayer.ps1
@@ -84,43 +84,42 @@ Add-Type -AssemblyName PresentationFramework, System.Drawing, System.Windows.For
 Add-Type -TypeDefinition 'using System.Runtime.InteropServices;
 [Guid("5CDF2C82-841E-4546-9722-0CF74078229A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 interface IAudioEndpointVolume {
-  int f(); int g(); int h(); int i();
-  int SetMasterVolumeLevelScalar(float fLevel, System.Guid pguidEventContext);
-  int j();
-  int GetMasterVolumeLevelScalar(out float pfLevel);
-  int k(); int l(); int m(); int n();
-  int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, System.Guid pguidEventContext);
-  int GetMute(out bool pbMute);
+	int f(); int g(); int h(); int i();
+	int SetMasterVolumeLevelScalar(float fLevel, System.Guid pguidEventContext);
+	int j();
+	int GetMasterVolumeLevelScalar(out float pfLevel);
+	int k(); int l(); int m(); int n();
+	int SetMute([MarshalAs(UnmanagedType.Bool)] bool bMute, System.Guid pguidEventContext);
+	int GetMute(out bool pbMute);
 }
 [Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 interface IMMDevice {
-  int Activate(ref System.Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
+	int Activate(ref System.Guid id, int clsCtx, int activationParams, out IAudioEndpointVolume aev);
 }
 [Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
 interface IMMDeviceEnumerator {
-  int f();
-  int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
+	int f();
+	int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);
 }
 [ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }
-
 public class Audio {
-  static IAudioEndpointVolume Vol() {
-var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
-IMMDevice dev = null;
-Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(/*eRender*/ 0, /*eMultimedia*/ 1, out dev));
-IAudioEndpointVolume epv = null;
-var epvid = typeof(IAudioEndpointVolume).GUID;
-Marshal.ThrowExceptionForHR(dev.Activate(ref epvid, /*CLSCTX_ALL*/ 23, 0, out epv));
-return epv;
-  }
-  public static float Volume {
-get {float v = -1; Marshal.ThrowExceptionForHR(Vol().GetMasterVolumeLevelScalar(out v)); return v;}
-set {Marshal.ThrowExceptionForHR(Vol().SetMasterVolumeLevelScalar(value, System.Guid.Empty));}
-  }
-  public static bool Mute {
-get { bool mute; Marshal.ThrowExceptionForHR(Vol().GetMute(out mute)); return mute; }
-set { Marshal.ThrowExceptionForHR(Vol().SetMute(value, System.Guid.Empty)); }
-  }
+	static IAudioEndpointVolume Vol() {
+		var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;
+		IMMDevice dev = null;
+		Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(/*eRender*/ 0, /*eMultimedia*/ 1, out dev));
+		IAudioEndpointVolume epv = null;
+		var epvid = typeof(IAudioEndpointVolume).GUID;
+		Marshal.ThrowExceptionForHR(dev.Activate(ref epvid, /*CLSCTX_ALL*/ 23, 0, out epv));
+		return epv;
+	}
+	public static float Volume {
+		get {float v = -1; Marshal.ThrowExceptionForHR(Vol().GetMasterVolumeLevelScalar(out v)); return v;}
+		set {Marshal.ThrowExceptionForHR(Vol().SetMasterVolumeLevelScalar(value, System.Guid.Empty));}
+	}
+	public static bool Mute {
+		get { bool mute; Marshal.ThrowExceptionForHR(Vol().GetMute(out mute)); return mute; }
+		set { Marshal.ThrowExceptionForHR(Vol().SetMute(value, System.Guid.Empty)); }
+	}
 }'
 [xml]$xaml='
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
@@ -262,8 +261,8 @@ $MenuFolder.Add_Click({
 		$files+=$_
 	}
 	TogglePlayButton
-	while($global:icurrent -lt $files.Length + 1){
-			NextTrack
+	while(([ref] $script:icurrent).Value -lt $files.Length - 1){
+		NextTrack
 	}
 })
 $MenuExit=$Window.FindName("Exit")
