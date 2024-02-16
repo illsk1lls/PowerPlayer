23b3217d2342ed41b610c93eea0dc165ff90721e
illsk1lls <gene.rovinsky@gmail.com>
2024-02-15 21:31:25 -0500

    Better folder picker

--- PowerPlayer.ps1
+++ PowerPlayer.ps1
@@ -249,11 +249,33 @@ $MenuFolder.Add_MouseLeave({
 })
 $MenuFolder.Add_Click({
 	dropDownMenu
-	$folder = New-Object System.Windows.Forms.FolderBrowserDialog
-	$folder.SelectedPath = "$env:UserProfile\Music"
-	$folderPicker = $folder.ShowDialog()
-	$path = $folder.SelectedPath
-	if($folderPicker -ne [System.Windows.Forms.DialogResult]::Cancel){
+	$AssemblyFullName = 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
+	$Assembly = [System.Reflection.Assembly]::Load($AssemblyFullName)
+	$OpenFileDialog = [System.Windows.Forms.OpenFileDialog]::new()
+	$OpenFileDialog.AddExtension = $false
+	$OpenFileDialog.CheckFileExists = $false
+	$OpenFileDialog.DereferenceLinks = $true
+	$OpenFileDialog.Filter = "Folders|`n"
+	$OpenFileDialog.Multiselect = $false
+	$OpenFileDialog.Title = "Select a Folder"
+	$OpenFileDialog.InitialDirectory="$env:UserProfile\Music"
+	$OpenFileDialogType = $OpenFileDialog.GetType()
+	$FileDialogInterfaceType = $Assembly.GetType('System.Windows.Forms.FileDialogNative+IFileDialog')
+	$IFileDialog = $OpenFileDialogType.GetMethod('CreateVistaDialog',@('NonPublic','Public','Static','Instance')).Invoke($OpenFileDialog,$null)
+	$OpenFileDialogType.GetMethod('OnBeforeVistaDialog',@('NonPublic','Public','Static','Instance')).Invoke($OpenFileDialog,$IFileDialog)
+	[uint32]$PickFoldersOption = $Assembly.GetType('System.Windows.Forms.FileDialogNative+FOS').GetField('FOS_PICKFOLDERS').GetValue($null)
+	$FolderOptions = $OpenFileDialogType.GetMethod('get_Options',@('NonPublic','Public','Static','Instance')).Invoke($OpenFileDialog,$null) -bor $PickFoldersOption
+	$FileDialogInterfaceType.GetMethod('SetOptions',@('NonPublic','Public','Static','Instance')).Invoke($IFileDialog,$FolderOptions)
+	$VistaDialogEvent = [System.Activator]::CreateInstance($AssemblyFullName,'System.Windows.Forms.FileDialog+VistaDialogEvents',$false,0,$null,$OpenFileDialog,$null,$null).Unwrap()
+	[uint32]$AdviceCookie = 0
+	$AdvisoryParameters = @($VistaDialogEvent,$AdviceCookie)
+	$AdviseResult = $FileDialogInterfaceType.GetMethod('Advise',@('NonPublic','Public','Static','Instance')).Invoke($IFileDialog,$AdvisoryParameters)
+	$AdviceCookie = $AdvisoryParameters[1]
+	$Result = $FileDialogInterfaceType.GetMethod('Show',@('NonPublic','Public','Static','Instance')).Invoke($IFileDialog,[System.IntPtr]::Zero)
+	$FileDialogInterfaceType.GetMethod('Unadvise',@('NonPublic','Public','Static','Instance')).Invoke($IFileDialog,$AdviceCookie)
+	if ($Result -ne [System.Windows.Forms.DialogResult]::Cancel) {
+		$FileDialogInterfaceType.GetMethod('GetResult',@('NonPublic','Public','Static','Instance')).Invoke($IFileDialog,$null)
+		$path = $OpenFileDialog.FileName+'\'
 		$files=@()
 		Get-ChildItem -Path $path -Filter *.mp3 -File -Name| ForEach-Object {
 			$files+=$_
